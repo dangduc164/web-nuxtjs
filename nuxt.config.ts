4df169d53570a03795d83a48ba2580503788cf04
Nguyễn Đăng Đức <ducdev2k1@gmail.com>
2025-01-18 22:24:34 +0700

    add YAML plugin for Rollup and create initial index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import yaml from '@rollup/plugin-yaml'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  vite: {
+    plugins: [
+      yaml()
+    ]
+  }
 })
